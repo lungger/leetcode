74ee00ec82063298f5009ec353a22fffe894d43f
lungger <35425012+lungger@users.noreply.github.com>
2025-10-09 17:07:12 +0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (66.65%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,19 +3,13 @@ public:
     bool isSubsequence(string s, string t) {
         int m = s.size(), n = t.size();
         int subFlag = 0, oriFlag = 0;
-        while (subFlag < m || oriFlag < n) {
-            while (oriFlag < n && s[subFlag] != t[oriFlag]) {
-                oriFlag++;
+        while (subFlag < m && oriFlag < n) {
+            if(s[subFlag] == t[oriFlag]) {
+                subFlag++;
             }
-            if (oriFlag >= n) {
-                break;
-            }
-            subFlag++;
             oriFlag++;
         }
-        if (subFlag != m) {
-            return false;
-        }
-        return true;
+        
+        return subFlag == m;
     }
 };
