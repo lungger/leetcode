d5f1ba4ee8da4b49dd576fe09baa68b3113300c7
lungger <35425012+lungger@users.noreply.github.com>
2025-10-20 18:51:04 +0800

    Time: 0 ms (100.00%) | Memory: 21.6 MB (79.37%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -7,15 +7,14 @@ public:
                 ans.push_back(i);
             }
             else {
-                while (ans.size() != 0 && ans.back() > 0 && ans.back() < i * -1) {
+                while (ans.size() != 0 && ans.back() > 0 && ans.back() < -i) {
                     ans.pop_back();
-                }
-                if (ans.size() != 0 && ans.back() == i * -1) {
-                    ans.pop_back();
-                    continue;
                 }
                 if (ans.size() == 0 || ans.back() < 0) {
                     ans.push_back(i);
+                }
+                if (ans.size() != 0 && ans.back() == -i) {
+                    ans.pop_back();
                 }
             }
         }
