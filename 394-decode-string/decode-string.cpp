26fdee1f03e5a52e4ee7d29e8ce0f71eb7d1c969
lungger <35425012+lungger@users.noreply.github.com>
2025-10-22 00:22:59 +0800

    Time: 2 ms (22.78%) | Memory: 9.4 MB (59.89%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int>  numStack;
+        stack<string> stringStack;
+        string tempS = "";
+        int num = 0;
+
+        for (auto c : s) {
+            if (c >= 'a' && c <= 'z') {
+                tempS += c;
+            }
+            else if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '[') {
+                numStack.push(num);
+                num = 0;
+                stringStack.push(tempS);
+                tempS = "";
+            }
+            else if (c == ']') {
+                num = numStack.top();
+                numStack.pop();
+                if (tempS == "") {
+                    tempS = stringStack.top();
+                    stringStack.pop();
+                }
+                string t = stringStack.top();
+                stringStack.pop();
+                while (num > 0) {
+                    t += tempS;
+                    num--;
+                }
+                tempS = t;
+            }
+        }
+        if (!stringStack.empty()) {
+            tempS = stringStack.top();
+        }
+
+        return tempS;
+    }
+};
