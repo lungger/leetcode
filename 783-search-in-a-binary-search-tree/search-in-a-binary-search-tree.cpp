2fdaeaf424a75ce67bde30b1b1ff1f697eeb311c
lungger <35425012+lungger@users.noreply.github.com>
2025-11-27 20:44:50 +0800

    Time: 0 ms (100.00%) | Memory: 35.4 MB (92.95%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -12,17 +12,10 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()) {
-            TreeNode* node = q.front(); q.pop();
-            if (node->val == val) {
-                return node;
-            }
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
+        while(root && root->val != val) {
+            root = root->val > val ? root->left : root->right;
         }
 
-        return NULL;
+        return root;
     }
 };
