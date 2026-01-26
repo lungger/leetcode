5a9af2a6f505549e6ba1891ad39343db9896efb1
lungger <35425012+lungger@users.noreply.github.com>
2026-01-27 01:54:08 +0800

    Time: 79 ms (94.59%) | Memory: 105.9 MB (82.22%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int count = 0;
+
+    void dfs(int node, int parant, vector<vector<pair<int,int>>>& adj) {
+        for (auto&  [neighbor, sign] : adj[node]) {
+            if (neighbor != parant) {
+                if (sign) {
+                    count++;
+                }
+                dfs(neighbor, node, adj);
+            }
+
+        }
+    }
+
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto& connection : connections) {
+            adj[connection[0]].push_back({connection[1], 1});
+            adj[connection[1]].push_back({connection[0], 0});
+        }
+        dfs (0, -1, adj);
+        return count;
+    }
+};
