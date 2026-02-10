fdc990b08595c5172e75ceee7143bd189d41a0bd
lungger <35425012+lungger@users.noreply.github.com>
2026-02-11 04:51:52 +0800

    Time: 0 ms (100.00%) | Memory: 9.2 MB (24.63%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -12,17 +12,9 @@ public:
         
         long long sum = dp[0];  // 累積和：dp[0] + dp[1] + ... + dp[i-3]
         
-        // for (int i = 3; i <= n; ++i) {
-        //     dp[i] = (dp[i-1] + dp[i-2] + 2 * sum) % MOD;
-        //     sum = (sum + dp[i-2]) % MOD;  // 更新累積和
-        // }
         for (int i = 3; i <= n; ++i) {
-            dp[i] = (dp[i-1] + dp[i-2]) % MOD;
-            
-            // 加上 2 * (dp[i-3] + dp[i-4] + ... + dp[0])
-            for (int j = i - 3; j >= 0; --j) {
-                dp[i] = (dp[i] + 2 * dp[j]) % MOD;
-            }
+            dp[i] = (dp[i-1] + dp[i-2] + 2 * sum) % MOD;
+            sum = (sum + dp[i-2]) % MOD;  // 更新累積和
         }
         
         return dp[n];
